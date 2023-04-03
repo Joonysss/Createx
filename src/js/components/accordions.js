fca3b5a73a88b19b09b27e6f845e17c08aded4db
Joonys <yura.klecko@gmail.com>
2023-04-03 11:02:47 -0500

    feat: add srvices-benefits section && styles

--- src/js/components/accordions.js
+++ src/js/components/accordions.js
@@ -59,12 +59,14 @@ class GraphAccordion {
 
 
 
-const accordion1 = new GraphAccordion('.accordion-1', {
-	speed: 300
-});
 
-const accordion2 = new GraphAccordion('.accordion-2', {
-	speed: 300
-});
 
-console.log(1);
+if (document.querySelector('.we-offer')) {
+	const accordion1 = new GraphAccordion('.accordion-1', {
+		speed: 300
+	});
+	
+	const accordion2 = new GraphAccordion('.accordion-2', {
+		speed: 300
+	});
+}
